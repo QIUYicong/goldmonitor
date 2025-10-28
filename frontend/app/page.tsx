d4ec9d4581eb9db9b27158a66494e52b8d909188
JOEY <lianyichuan@foxmail.com>
2025-10-28 21:06:21 +0800

    Fix: Remove duplicate closing table tag

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -178,7 +178,7 @@ export default function Home() {
                     </tbody>
                   </table>
                 </div>
-              </table>
+              </div>
             </section>
           </>
         )}
